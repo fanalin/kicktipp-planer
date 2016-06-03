3caa7621d2e141934dcfeaf834706c5fc1922334
Matti Mäkitalo <matti.maekitalo@gmail.com>
2016-06-03 17:28:55 +0000

    Added more stuff
     - firebase configuration
     - Google+ login-buttons
     - Sass source files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,42 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    connect: {
-        server : {
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    hostname: '10.0.2.15',
+                    port: 3000,
+                    base: 'src',
+                    livereload: true
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    loadPath: 'src/vendor/bootstrap/scss'
+                },
+                files: {
+                    'src/css/main.css': 'src/scss/main.scss'
+                }
+            }
+        },
+        watch: {
             options: {
-                hostname: '10.0.2.15',
-                port: 3000,
-                base: 'src',
-                livereload: false,
-                keepalive: true
+                spawn: false,
+                livereload: true
+            },
+            css: {
+                files: ['src/scss/*.scss'],
+                tasks: ['sass']
             }
         }
-    }
 
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['connect']);
+    grunt.registerTask('default', ['connect', 'watch']);
 };
