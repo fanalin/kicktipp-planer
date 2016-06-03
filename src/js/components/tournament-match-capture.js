d001350e2c384c23e7077eae8d7315b1656804f6
Matti Mäkitalo <matti.maekitalo@gmail.com>
2016-06-03 18:46:22 +0000

    Introduce different match stages (not played, live, finished)
    Added match start/finish and show result in red in tournament overview if match
     is live

--- src/js/components/tournament-match-capture.js
+++ src/js/components/tournament-match-capture.js
@@ -3,70 +3,21 @@ angular
     .module('tippkick-planer-app')
     .component('tournamentMatchCapture', {
         templateUrl: '/js/components/tournament-match-capture.html',
-        controller: ['$rootScope', '$attrs', 'firebase', '$location', function ($rootScope, $attrs, firebase, $location) {
+        controller: ['$attrs', 'tournamentMatchRepository', '$location', function ($attrs, tournamentMatchRepository, $location) {
             var that = this;
 
             that.tournamentId = $attrs.tournamentId;
 
-            var key = '/tournaments/' + $attrs.tournamentId
-                + '/groups/' + $attrs.groupId
-                + '/matches/' + $attrs.matchId;
+            that.match = tournamentMatchRepository.get(
+                $attrs.tournamentId,
+                $attrs.groupId,
+                $attrs.matchId
+            );
 
-            var matchRef = firebase.database().ref(key);
-
-            matchRef.once('value', function(match) {
-                that.match = match.val();
-                $rootScope.safeApply();
-            });
-
-            this.addGoal = function(player) {
-                player.goals++;
+            that.finishMatch = function() {
+                that.matchData.finishMatch();
+                $location.path('/tournament/' + that.tournamentId);
             };
-            this.removeGoal = function(player) {
-                if (player.goals < 1) {
-                    return;
-                }
-                player.goals--;
-            };
-
-            this.captureResult = function() {
-                that.match.played = true;
-                matchRef.set(angular.copy(that.match));
-
-                recalculateStandings(that.match);
-
-                $location.path('/tournament/' + $attrs.tournamentId);
-            };
-
-            function recalculateStandings(playedMatch) {
-                var playersKey = '/tournaments/' + $attrs.tournamentId + '/groups/' + $attrs.groupId + '/players';
-                var playersRef = firebase.database().ref(playersKey);
-
-                playersRef.once('value', function(playersSnapshot) {
-                    var players = playersSnapshot.val();
-                    angular.forEach(players, function(player) {
-                        if (player.name == playedMatch.home.name) {
-                            addToPlayer(player, playedMatch.home.goals, playedMatch.away.goals);
-                        } else if (player.name == playedMatch.away.name) {
-                            addToPlayer(player, playedMatch.away.goals, playedMatch.home.goals);
-                        }
-                    });
-                    playersRef.set(players);
-                });
-
-                function addToPlayer(player, goalsScored, goalsAgainst) {
-                    player.goals += goalsScored;
-                    player.goalsAgainst += goalsAgainst;
-                    player.goalDiff = goalsScored - goalsAgainst;
-
-                    if (goalsScored > goalsAgainst) {
-                        player.points += 3;
-                    } else if (goalsScored == goalsAgainst) {
-                        player.points += 1;
-                    }
-
-                }
-            }
         } ]
     });
 
---
+++ src/js/services/TournamentMatchService.js
@@ -0,0 +1,85 @@
+(function(mod) {
+
+    mod.factory('tournamentMatchRepository', ['firebase', function (firebase) {
+        return {
+            get : function(tournamentId, groupId, matchId) {
+                return new Match(firebase, tournamentId, groupId, matchId);
+            }
+        }
+    }]);
+
+    function Match(firebase, tournamentId, groupId, matchId) {
+        var that = this;
+
+        var matchKey = '/tournaments/' + tournamentId
+            + '/groups/' + groupId
+            + '/matches/' + matchId;
+
+        firebase.database().ref(matchKey).on('value', function(snapshot) {
+            that.matchData = snapshot.val();
+        });
+
+        this.startMatch = function() {
+            // don't start match if it already began ('live') or is even finished ('finished')
+            if (!! that.matchData.played) {
+                return;
+            }
+            firebase.database().ref(matchKey + '/played').set('live');
+        };
+
+        this.finishMatch = function() {
+            firebase.database().ref(matchKey + '/played').set('finished');
+            recalculateStandings();
+        };
+        this.addGoalToHome = function() {
+            var newGoalAmount = that.matchData.home.goals + 1;
+            firebase.database().ref(matchKey + '/home/goals').set(newGoalAmount);
+        };
+        this.addGoalToAway = function() {
+            var newGoalAmount = that.matchData.away.goals + 1;
+            firebase.database().ref(matchKey + '/away/goals').set(newGoalAmount);
+        };
+        this.removeGoalFromHome = function() {
+            var newGoalAmount = that.matchData.home.goals - 1;
+            if (newGoalAmount < 0) {
+                return;
+            }
+            firebase.database().ref(matchKey + '/home/goals').set(newGoalAmount);
+        };
+        this.removeGoalFromAway = function() {
+            var newGoalAmount = that.matchData.away.goals - 1;
+            if (newGoalAmount < 0) {
+                return;
+            }
+            firebase.database().ref(matchKey + '/away/goals').set(newGoalAmount);
+        };
+
+        function addToPlayer(player, goalsScored, goalsAgainst) {
+            player.goals += goalsScored;
+            player.goalsAgainst += goalsAgainst;
+            player.goalDiff = goalsScored - goalsAgainst;
+
+            if (goalsScored > goalsAgainst) {
+                player.points += 3;
+            } else if (goalsScored == goalsAgainst) {
+                player.points += 1;
+            }
+        }
+
+        function updatePlayer(playerId, goalsScored, goalsAgainst) {
+            var playerKey = '/tournaments/' + tournamentId + '/groups/' + groupId + '/players/' + playerId;
+            var playerRef = firebase.database().ref(playerKey);
+            playerRef.once('value', function (playerSnapshot) {
+                var player = playerSnapshot.val();
+                addToPlayer(player, goalsScored, goalsAgainst);
+            });
+        }
+
+        function recalculateStandings() {
+            updatePlayer(that.matchData.home.id, that.matchData.home.goals, that.matchData.away.goals);
+            updatePlayer(that.matchData.away.id, that.matchData.away.goals, that.matchData.home.goals);
+        }
+    }
+
+
+})(angular.module('tippkick-planer-app'));
